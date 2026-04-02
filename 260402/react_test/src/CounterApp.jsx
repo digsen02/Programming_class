9f8446299ae858b551d1ee2bfde4ce7f2765187e
user <dlgusdnse2008@gmail.com>
2026-04-02 09:53:22 +0900

    CountApp +

--- 260402/react_test/src/CounterApp.jsx
+++ 260402/react_test/src/CounterApp.jsx
@@ -1,19 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 
 function CounterApp() {
+  //count: 상태 값, 이전 상태값; set Count: 상태를 업데이트하는 함수. 반드시 얘로 바꿔야함 (직접 count = 1 이런식으로 바꾸면 안됨)
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <button
-      className='counter'
-      onClick={()=>setCount((count) => count +1)}
-    >
-      Count is {count}
-    </button>
+      <h1>{count}</h1>
+      <button onClick={()=>setCount((count) => count +1)}>+</button>
+    {
+    // <button
+    //   className='counter'
+    //   onClick={()=>setCount((count) => count +1)}
+    // >
+    //   Count is {count}
+    // </button>
+    }
     </>
   )
 }
