a7aca4b46f7138d327ceea9f874d261ed8387d49
user <dlgusdnse2008@gmail.com>
2026-04-09 09:07:19 +0900

    CounterApp + (최대10까지)

--- 260402/react_test/src/CounterApp.jsx
+++ 260402/react_test/src/CounterApp.jsx
@@ -1,8 +1,16 @@
+//https://programming.dlgusdnse2008.workers.dev/
+
 import { useState } from 'react'
 
 function CounterApp() {
   //count: 상태 값, 이전 상태값; set Count: 상태를 업데이트하는 함수. 반드시 얘로 바꿔야함 (직접 count = 1 이런식으로 바꾸면 안됨)
   const [count, setCount] = useState(0)
+  function plusMax10(){
+    setCount((count)=>{
+      if (count >= 10) return 10;
+      return count +1;
+    })
+  }
 
   return (
     <>
@@ -11,7 +19,12 @@ function CounterApp() {
       <button onClick={()=>setCount((count) => count +2)}>+2</button>
       <button onClick={()=>setCount((count) => count -1)}>-</button>  
       <button onClick={()=>setCount(0)}>reset</button>  
-    
+      <button onClick={()=>setCount((count) => count >= 10 ? 10 : count +1 )}>+(최대 10까지)</button>  
+      <button onClick={()=>setCount((count) => Math.min(count + 1, 10) )}>+(최대 10까지)</button>
+      <button onClick={()=>plusMax10()}>+(최대 10까지)</button>
+      <button onClick={plusMax10}>+(최대 10까지)</button>
+
+
     </>
   )
 }
