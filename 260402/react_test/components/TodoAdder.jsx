4526ea6087d3a6093e5405a45a227cf44564e8ee
user <dlgusdnse2008@gmail.com>
2026-04-17 14:28:54 +0900

    TodoListApp addTodo()

--- 260402/react_test/components/TodoAdder.jsx
+++ 260402/react_test/components/TodoAdder.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react'    
 import Button from './Button.jsx'
 
-export default function TodoAdder() {
+export default function TodoAdder({addTodo}) {
+    const [inputTodo, setInputTodo] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault(); //submit 이벤트의 기본 동작인 새로고침 방지
+        
+        if(inputTodo.trim() === '') return; //공백만 입력한 경우 추가하지 않음
+        addTodo(inputTodo.trim()); //부모 컴포넌트로부터 전달받은 addTodo 함수 호출하여 새로운 할 일 추가
+
+        setInputTodo('');
+    }
 
     return (
         <>
-            <form className="todo__form">
-                <input type="text" name="" id="" className="todo__input" placeholder='할 일을 입력하세요.' />
+            <form className="todo__form" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    className="todo__input"
+                    placeholder='할 일을 입력하세요.'
+                    value={inputTodo}
+                    onChange={(event) => setInputTodo(event.target.value)}
+                />
                 <Button type="submit" className="todo__button todo__button--add">Add</Button>
             </form>
         </>
--- 260402/react_test/src/TodoListApp.jsx
+++ 260402/react_test/src/TodoListApp.jsx
@@ -1,16 +1,33 @@
+import { useState } from 'react'
+
 import './todoListApp.css'
 import TodoHeader from '../components/TodoHeader.jsx'
 import TodoAdder from '../components/TodoAdder.jsx'
 import TodoList from '../components/TodoList.jsx'
 
-function TodoListApp() {
+class Todo {
+    constructor(text) {
+        this.id = Date.now() //pk
+        this.text = text //할일 내용
+        this.isCompleted = false //완료 여부
+    }
+}
 
+function TodoListApp() {
+    const[todos, setTodos] = useState([])
+    
+    function addTodo(text) {
+        setTodos((todos) => [
+            ...todos,
+            new Todo(text)
+        ])
+    }
 
     return(
         <div className="todo">
             <TodoHeader/>
 
-            <TodoAdder/>
+            <TodoAdder addTodo={addTodo} />
 
             <TodoList/>
 
