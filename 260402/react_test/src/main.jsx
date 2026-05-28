eef0948b0a7d76867d9159d2e9b4c850b2ec1dd8
user <dlgusdnse2008@gmail.com>
2026-05-28 09:45:55 +0900

    RouterApp VrowserRouter, Routes, Route, Link

--- 260402/react_test/src/main.jsx
+++ 260402/react_test/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import TodoListApp from './TodoListApp.jsx'
 import HomeApp from './HomeApp.jsx'
 import './reset.css'
+import RouterApp from './RouterApp.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HomeApp />
+    <RouterApp />
   </StrictMode>,
 )
